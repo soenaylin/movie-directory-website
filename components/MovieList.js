8f647a423484c3c3cfcc818e5ee97b0c6e9fc255
Soe Nay Lin <soenaylin.g@gmail.com>
2025-09-24 10:25:09 +0630

    movie directory website

---
+++ components/MovieList.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function MovieList({ movies }) {
+  const poster = "http://image.tmdb.org/t/p/w342";
+
+  return (
+    <div className="flex flex-wrap gap-4 justify-evenly">
+      {movies.map((movie) => {
+        return (
+          <div className="w[200px] text-center" key={movie.id}>
+            <Link href={`/movie/${movie.id}`}>
+              <img className="hover:scale-105 transition-all" src={poster + movie.poster_path} />
+            </Link>
+            <div className="mt-2">{movie.title}</div>
+            <span className="text-gray-600">
+              {movie.release_date.split("-")[0]}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
